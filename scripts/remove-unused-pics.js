4bff7ff12a95dcfd973c3e9c361be0cc4c954d06
Adam Vesecky <vesecky.adam@gmail.com>
2023-12-17 21:21:27 +0100

    Remove unused pics

---
+++ scripts/remove-unused-pics.js
@@ -0,0 +1,22 @@
+// this file lists all files that are not referenced from any of the pug files
+
+const fs = require("fs");
+const path = require("path");
+const utils = require('./utils');
+
+const allPugFiles = utils.searchFiles('slides', ['.pug']);
+
+let pugContent = '';
+for(let file of allPugFiles) {
+    pugContent += utils.fileToStr(file);
+}
+
+const allImages = utils.searchFiles('slides/assets', ['.png', '.svg', '.gif', '.jpg']);
+
+for(let img of allImages) {
+    const fileName = path.basename(img);
+    if(pugContent.indexOf(fileName) === -1) {
+        utils.deleteFile(img);
+        console.log(fileName);
+    }
+}
